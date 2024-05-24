2c0347cd30d280926300e4b4543e1374167420fb
aditya94139 <152467027+aditya94139@users.noreply.github.com>
2024-05-24 14:33:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ bot = Client("bot",
              bot_token=os.environ.get("BOT_TOKEN"),
              api_id=int(os.environ.get("API_ID")),
              api_hash=os.environ.get("API_HASH"))
-auth_users = [6693536228,1511103739,5684410709,6034357260,6488555238]
+auth_users = [5827915041,1368753935,6305120550]
 sudo_users = auth_users
 sudo_groups = [-1002065091813]
 
